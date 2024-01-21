a2323238a7b5f9d747c58c2df5208f5e301626f6
kylee <giseggi57@gmail.com>
2024-01-21 22:45:51 +0900

    DFS1

---
+++ src/graphtraversal/DFS1.java
@@ -0,0 +1,53 @@
+package graphtraversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class DFS1 {
+
+    static int count;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int visited[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        visited = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph.get(x).add(y);
+            graph.get(y).add(x);
+        }
+        for (int i = 1; i <= N; i++) Collections.sort(graph.get(i));
+        count = 1;
+        DFS(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < visited.length; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void DFS(int R) {
+        visited[R] = count;
+        for (int i : graph.get(R)) {
+            if (visited[i] == 0) {
+                count++;
+                DFS(i);
+            }
+        }
+    }
+}
